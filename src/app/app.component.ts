2b0b60a5d62e2986c284564315547ce8836a4d37
Tieu Cam Thanh <tieucamthanh@vigilantsolutions.com>
2023-02-09 20:58:36 +0700

    working with subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shopping-app'
-
-  loadedFeature = 'recipe'
-
-  onNavigate(feature: string): void {
-    this.loadedFeature = feature
-  }
 }
